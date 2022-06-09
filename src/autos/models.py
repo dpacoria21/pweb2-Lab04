a43aeecefa2a5818abdff0b8b4d0d7cbb7d3d9bf
diego pacori <dpacoria@unsa.edu.pe>
2022-06-08 23:22:36 -0500

    Se cambio el espacio para la placa a 8

--- src/autos/models.py
+++ src/autos/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Auto(models.Model):
     marca = models.TextField()
-    placa = models.CharField(max_length=5)
+    placa = models.CharField(max_length=8)
     tiempoUso = models.IntegerField()
