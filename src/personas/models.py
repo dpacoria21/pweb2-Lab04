032e8528aa28fded1006a1fa389fa656589c1d94
diego pacori <dpacoria@unsa.edu.pe>
2022-06-09 21:04:00 -0500

    Al agregar un nuevo campo el mismo django me pide agregar un valor default y le di a True, por lo tanto los demas objetos tendrian esta caracteristica activada (yo elegi la opcion True, tambien pude haber elegido False)

--- src/personas/models.py
+++ src/personas/models.py
@@ -7,3 +7,4 @@ class Persona(models.Model):
     nombre = models.CharField(max_length = 100)
     apellidos = models.CharField(max_length = 100)
     edad = models.IntegerField()
+    donador = models.BooleanField()
